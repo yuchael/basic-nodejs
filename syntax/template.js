529db36cbf0dcb4dc7bb8c53501e0a296e09fa88
yuchael <97613473+yuchael@users.noreply.github.com>
2023-02-13 00:46:58 +0900

    nodejs start

---
+++ syntax/template.js
@@ -0,0 +1,7 @@
+var name = 'k8805'
+var letter = 'Dear '+name+'\nLoem ipsum dolor sit amet, ~~~';
+var letter = `Dear ${name}
+
+Loem ipsum dolor sit amet, ~~~ ${1+1}`;
+console.log(letter);
+//"http(protocol)://opentutorials.org(host):3000(port)/main(path)?id=HTML&page=12(query string, 시작은 ?, 값과 값은 &로 구분, 값의 이름과 값은 =로 구분)"
